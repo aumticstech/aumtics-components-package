7e23eb12e57f921ea36a9cea5f45b10e9ce901ae
Ravi <ravis@Aumticss-Mac-mini.local>
2025-08-06 15:51:18 +0530

    chabgs

--- src/resources/views/components/alert.blade.php
+++ src/resources/views/components/alert.blade.php
@@ -1,6 +1,6 @@
 @props(['message','type'])
 
-<div class="alert testdata alert-{{ $type }} light-mode mb-4">
+<div class="alert testdatanew alert-{{ $type }} light-mode mb-4">
     <div class="flex items-center space-x-3 rtl:space-x-reverse">
         <iconify-icon class="text-2xl flex-0" icon="system-uicons:target"></iconify-icon>
         <p class="flex-1 font-Inter">{{ $message }}</p>
